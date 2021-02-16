36c39fcf939e49149e8be25254aa6e4bf2a6207b
“wu_chengyan” <2280030785@qq.com>
2021-02-16 17:55:28 +0800

    第一次提交

---
+++ frontend/src/products.js
@@ -0,0 +1,52 @@
+const products = [
+  {
+    _id: '1',
+    name: '苹果12 mini 64G',
+    image: '/images/demo.jpg',
+    description: '万千人为之疯狂',
+    brand: '苹果',
+    category: '电子产品',
+    price: 5499,
+    countInStock: 10,
+    rating: 4.0,
+    numReviews: 5, 
+  },
+  {
+    _id: '2',
+    name: '苹果12 mini 64G',
+    image: '/images/demo.jpg',
+    description: '万千人为之疯狂',
+    brand: '苹果',
+    category: '电子产品',
+    price: 5499,
+    countInStock: 10,
+    rating: 4.0,
+    numReviews: 5, 
+  },
+  {
+    _id: '3',
+    name: '苹果12 mini 64G',
+    image: '/images/demo.jpg',
+    description: '万千人为之疯狂',
+    brand: '苹果',
+    category: '电子产品',
+    price: 5499,
+    countInStock: 10,
+    rating: 4.0,
+    numReviews: 5, 
+  },
+  {
+    _id: '4',
+    name: '苹果12 mini 64G',
+    image: '/images/demo.jpg',
+    description: '万千人为之疯狂',
+    brand: '苹果',
+    category: '电子产品',
+    price: 5499,
+    countInStock: 10,
+    rating: 4.0,
+    numReviews: 5, 
+  }
+]
+
+export default products;
